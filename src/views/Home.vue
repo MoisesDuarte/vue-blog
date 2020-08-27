178c142e57a8d1d8f6b6e434bddf6366609f4473
MoisesDuarte <moisesduartejunior@gmail.com>
2020-08-27 19:01:51 -0300

    Listing posts in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <b-container fluid>  
-    <b-card title="Blog Title" sub-title="Blog Author" class="mt-3">
+    <b-card v-for="blog in blogs" :key="blog.id" :title="blog.title" :sub-title="blog.author" class="mt-3 mb-3">
       <b-card-text>
-        <p><b-badge pill variant="primary">Category</b-badge></p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc rutrum tellus sed tellus luctus ullamcorper. Nam ut condimentum arcu, vitae accumsan erat. Maecenas ullamcorper magna eget ante tristique fringilla. Proin sit amet enim et nisi rutrum elementum. Duis felis orci, imperdiet a consectetur et, sollicitudin et lorem. Integer mauris mi, ultrices ac congue at, molestie ut risus. Ut pellentesque leo justo, vitae ultrices risus ultricies a. Ut ante mauris, laoreet id auctor pulvinar, lobortis vel lorem. Aenean tincidunt et justo non fringilla. Vivamus quis pretium dui, et vulputate enim. Suspendisse vehicula auctor maximus. </p>
+        <p><b-badge v-for="category in blog.categories" :key='category' pill variant="primary">{{category}}</b-badge></p>
+        <p>{{blog.content}}</p>
       </b-card-text>
     </b-card>
   </b-container>
@@ -12,6 +12,11 @@
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      blogs: []
+    }
+  },
   created() {
     this.Axios.get('http://localhost:3000/posts')
     .catch(err => {
@@ -19,6 +24,7 @@ export default {
     })
     .then(response => {
       console.log(response);
+      this.blogs = response.data.slice(0, 5);
     })
   }
 }
