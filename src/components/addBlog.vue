d267e160022b7ccef68e8334068ef9884cfabf5f
MoisesDuarte <moisesduartejunior@gmail.com>
2020-08-27 14:59:37 -0300

    Adding category checkboxes to addBlog.vue

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -1,7 +1,7 @@
 <template>
     <b-container fluid class="mt-4">   
         <!-- Blog Posting Form -->
-        <h2>Add a New Blog Post</h2>
+        <h2 class="lead text-center">Add a New Blog Post</h2>
         <b-form> 
             <!-- Title -->
             <b-form-group label="Blog Title:" label-for="blog-title">
@@ -11,10 +11,15 @@
             <b-form-group label="Blog Content:" label-for="blog-content">
                 <b-form-textarea id="blog-content" size="sm" v-model="blog.content" lazy></b-form-textarea>
             </b-form-group>
+            <!-- Category -->
+            <b-form-group label="Categories:">
+                <b-form-checkbox-group id="blog-category" v-model="blog.selectedCategories" :options="blog.categories"></b-form-checkbox-group>
+            </b-form-group>
+
         </b-form>
 
         <!-- Blog Posting Preview -->
-        <b-card header="Blog Preview" :title="blog.title">
+        <b-card header="Blog Preview" :title="blog.title" :subTitle="blog.selectedCategories.join(', ')">
             <b-card-text>{{ blog.content }}</b-card-text>
         </b-card>
     </b-container>
@@ -30,10 +35,22 @@
         data() {
             return {
                 blog: {
-                    title: "",
-                    content: ""
+                    title: '',
+                    content: '',
+                    selectedCategories: [],
+                    categories: [
+                        { text: 'Urban', value: 'urban' },
+                        { text: 'Pastoral', value: 'pastoral' },
+                        { text: 'Horror', value: 'horror' },
+                        { text: 'Thriller', value: 'thriller' },
+                        { text: 'Magical-Realism', value: 'mg-realism' },
+                    ]
                 }
             }
         }
     }
 </script>
+
+<style>
+    /*  */
+</style>
