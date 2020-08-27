1fd2c9be2e04ee04fa4c898c804378bea4f91f26
MoisesDuarte <moisesduartejunior@gmail.com>
2020-08-27 14:38:16 -0300

    Adding addBlog component + BootstrapVue Dependency

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <add-blog/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import addBlog from './components/addBlog.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    'add-blog': addBlog
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/addBlog.vue
@@ -0,0 +1,39 @@
+<template>
+    <b-container fluid class="mt-4">   
+        <!-- Blog Posting Form -->
+        <h2>Add a New Blog Post</h2>
+        <b-form> 
+            <!-- Title -->
+            <b-form-group label="Blog Title:" label-for="blog-title">
+                <b-form-input id="blog-title" v-model="blog.title" :state="titleState" lazy required></b-form-input>
+            </b-form-group>
+            <!-- Content -->
+            <b-form-group label="Blog Content:" label-for="blog-content">
+                <b-form-textarea id="blog-content" size="sm" v-model="blog.content" lazy></b-form-textarea>
+            </b-form-group>
+        </b-form>
+
+        <!-- Blog Posting Preview -->
+        <b-card header="Blog Preview" :title="blog.title">
+            <b-card-text>{{ blog.content }}</b-card-text>
+        </b-card>
+    </b-container>
+</template>
+
+<script>
+    export default {
+        computed: {
+            titleState() {
+                return this.blog.title.length > 2 ? true : false;
+            }
+        },
+        data() {
+            return {
+                blog: {
+                    title: "",
+                    content: ""
+                }
+            }
+        }
+    }
+</script>
