7578b99345f1de49675591b79c1c20cf82332c4f
MoisesDuarte <moisesduartejunior@gmail.com>
2020-08-27 15:16:07 -0300

    Adding author select to addBlog.vue

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -9,18 +9,24 @@
             </b-form-group>
             <!-- Content -->
             <b-form-group label="Blog Content:" label-for="blog-content">
-                <b-form-textarea id="blog-content" size="sm" v-model="blog.content" lazy></b-form-textarea>
+                <b-form-textarea id="blog-content" size="sm" v-model="blog.content" :state="contentState" lazy required></b-form-textarea>
             </b-form-group>
             <!-- Category -->
             <b-form-group label="Categories:">
                 <b-form-checkbox-group id="blog-category" v-model="blog.selectedCategories" :options="blog.categories"></b-form-checkbox-group>
             </b-form-group>
-
+            <!-- Author -->
+            <b-form-group label="Author:">
+                <b-form-select v-model="blog.selectedAuthor" :options="blog.authors"></b-form-select>
+            </b-form-group>
         </b-form>
 
         <!-- Blog Posting Preview -->
         <b-card header="Blog Preview" :title="blog.title" :subTitle="blog.selectedCategories.join(', ')">
-            <b-card-text>{{ blog.content }}</b-card-text>
+            <b-card-text>
+                <p class="text-muted">Authored by {{ blog.selectedAuthor }}</p>
+                <p>{{ blog.content }}</p>
+            </b-card-text>
         </b-card>
     </b-container>
 </template>
@@ -30,6 +36,9 @@
         computed: {
             titleState() {
                 return this.blog.title.length > 2 ? true : false;
+            },
+            contentState() {
+                return this.blog.content.length > 2 ? true : false;
             }
         },
         data() {
@@ -38,12 +47,19 @@
                     title: '',
                     content: '',
                     selectedCategories: [],
+                    selectedAuthor: null,
                     categories: [
                         { text: 'Urban', value: 'urban' },
                         { text: 'Pastoral', value: 'pastoral' },
                         { text: 'Horror', value: 'horror' },
                         { text: 'Thriller', value: 'thriller' },
                         { text: 'Magical-Realism', value: 'mg-realism' },
+                    ],
+                    authors: [
+                        { value: null, text: 'Please select an option' },
+                        { value: "M. Duarte", text: 'Moisés Duarte' },
+                        { value: "I. Lafayette", text: 'Ilia Lafayette' },
+                        { value: "Anon", text: 'Anonymous' },
                     ]
                 }
             }
