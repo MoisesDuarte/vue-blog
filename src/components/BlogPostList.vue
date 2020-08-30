67dddc5fb4708c99d02fa16e29979c434b401620
MoisesDuarte <moisesduartejunior@gmail.com>
2020-08-30 15:47:11 -0300

    Moving blog listing to a standalone component

---
+++ src/components/BlogPostList.vue
@@ -0,0 +1,34 @@
+<template> 
+    <div id="BlogPostList">
+        <b-card v-for="blog in blogs" :key="blog.id" :title="blog.title" :sub-title="blog.author" class="mt-3 mb-3">
+            <b-card-text>
+                <p><b-badge v-for="category in blog.categories" :key='category' pill variant="primary">{{category}}</b-badge></p>
+                <p>{{blog.content}}</p>
+            </b-card-text>
+        </b-card>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blogs: []
+    }
+  },
+  created() {
+    this.Axios.get('http://localhost:3000/posts')
+    .catch(err => {
+      console.log(err);
+    })
+    .then(response => {
+      console.log(response);
+      this.blogs = response.data.slice(0, 5);
+    })
+  }
+}    
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,16 @@
 <template>
-  <b-container fluid>  
-    <b-card v-for="blog in blogs" :key="blog.id" :title="blog.title" :sub-title="blog.author" class="mt-3 mb-3">
-      <b-card-text>
-        <p><b-badge v-for="category in blog.categories" :key='category' pill variant="primary">{{category}}</b-badge></p>
-        <p>{{blog.content}}</p>
-      </b-card-text>
-    </b-card>
+  <b-container fluid>
+    <BlogPostList />
   </b-container>
 </template>
 
 <script>
+import BlogPostList from '../components/BlogPostList.vue';
+
 export default {
   name: 'Home',
-  data() {
-    return {
-      blogs: []
-    }
-  },
-  created() {
-    this.Axios.get('http://localhost:3000/posts')
-    .catch(err => {
-      console.log(err);
-    })
-    .then(response => {
-      console.log(response);
-      this.blogs = response.data.slice(0, 5);
-    })
+  components: {
+    BlogPostList
   }
 }
 </script>
