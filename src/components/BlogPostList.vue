c0d0d6903f54c19c5125b892dad82d08c49b3d3c
MoisesDuarte <moisesduartejunior@gmail.com>
2020-08-30 16:41:47 -0300

    Implementing delete function in BlogPostList component

--- src/components/BlogPostList.vue
+++ src/components/BlogPostList.vue
@@ -5,7 +5,7 @@
                 <p><b-badge v-for="category in blog.categories" :key='category' pill variant="primary">{{category}}</b-badge></p>
                 <p>{{blog.content}}</p>
             </b-card-text>
-            <b-button variant="outline-danger" class="float-right"><b-icon-trash/> Delete</b-button>
+            <b-button v-on:click.prevent="deletePost(blog.id)" variant="outline-danger" class="float-right"><b-icon-trash/> Delete</b-button>
         </b-card>
     </div>
 </template>
@@ -18,14 +18,32 @@ export default {
     }
   },
   created() {
-    this.Axios.get('http://localhost:3000/posts?_sort=id&_order=desc')
-    .catch(err => {
-      console.log(err);
-    })
-    .then(response => {
-      console.log(response);
-      this.blogs = response.data.slice(0, 5);
-    })
+    this.getPosts();
+  },
+  methods: {
+      getPosts : function() {
+        this.Axios.get('http://localhost:3000/posts')
+        .catch(err => {
+            console.log(err);
+        })
+        .then(response => {
+            console.log(response);
+            this.blogs = response.data.slice(0, 5);
+        })
+      },
+      deletePost : function(blogId) {
+          this.Axios.delete(`http://localhost:3000/posts/${blogId}`)
+          .catch(err => {
+              console.log(err);
+          })
+          .then(response => {
+              console.log(response.data);
+
+              // Removing from local array
+              var removeIndex = this.blogs.map(function(item) { return item.id }).indexOf(blogId);
+              this.blogs.splice(removeIndex, 1);
+          })
+      }
   }
 }    
 </script>
